f48a7ec63306db6fa03a7e23fb839c8103053df3
Kevin Harris <kharris1@fossil.com>
2016-01-13 18:02:46 -0600

    New sample for embedding our views into a navigation controller.

---
+++ NavController/NavController/ModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModalViewController.swift
+//  NavController
+//
+//  Created by Kevin Harris on 1/12/16.
+//  Copyright © 2016 guildsa. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onTouchUpInsideDismiss(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: {
+            print("ModalViewController was dismissed!");
+            })
+    }
+}

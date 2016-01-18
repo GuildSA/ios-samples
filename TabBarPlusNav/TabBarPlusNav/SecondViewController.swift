a2be6779d0facff38531a617d4661a157230f37f
Kevin Harris <kharris1@fossil.com>
2016-01-18 13:13:43 -0600

    Add new sample that shows how to mix Tab Bars and Navigation Controllers.

---
+++ TabBarPlusNav/TabBarPlusNav/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBarPlusNav
+//
+//  Created by Kevin Harris on 1/18/16.
+//  Copyright © 2016 guildsa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

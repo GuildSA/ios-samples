c84556d66346b1758d9be190da843ad72cadacea
Kevin Harris <kharris1@fossil.com>
2016-01-18 15:53:48 -0600

    Add sample to demo UIStackView.

---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Kevin Harris on 1/18/16.
+//  Copyright © 2016 guildsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

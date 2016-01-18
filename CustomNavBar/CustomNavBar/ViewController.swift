a884ea11a25d0a244828420444da5fc1dd82d577
Kevin Harris <kharris1@fossil.com>
2016-01-18 11:49:45 -0600

    Add back samples with new directory layout.

---
+++ CustomNavBar/CustomNavBar/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CustomNavBar
+//
+//  Created by Kevin Harris on 1/11/16.
+//  Copyright © 2016 guildsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+//        // This sets the Back button text color for this View Controllers.
+//        let navigationBar = self.navigationController!.navigationBar
+//        navigationBar.tintColor = UIColor.whiteColor()
+//
+//        // This sets the text in the middle of the Nav Bar for this View Controllers.
+//        navigationItem.title = "My Login"
+//
+//        // This sets the background image of the whole Nav Bar for this View Controllers.
+//        let navBarBackgroundImage = UIImage(named: "Background Nav Bar")
+//        navigationBar.setBackgroundImage(navBarBackgroundImage, forBarMetrics: .Default)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

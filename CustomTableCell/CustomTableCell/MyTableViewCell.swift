bd070080b3fd69a978ac60b8bcb23f871037d069
Kevin Harris <kharris@guildsa.org>
2016-01-24 17:06:18 -0600

    Add new sample.

---
+++ CustomTableCell/CustomTableCell/MyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyTableViewCell.swift
+//  CustomTableCell
+//
+//  Created by Kevin Harris on 1/24/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CustomTableCell/CustomTableCell/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  CustomTableCell
+//
+//  Created by Kevin Harris on 11/5/15.
+//  Copyright © 2015 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+      
+    let myData = [
+        "Some data 1",
+        "Some data 2",
+        "Some data 3",
+        "Some data 4",
+        "Some data 5",
+        "Some data 6",
+        "Some data 7",
+        "Some data 8",
+        "Some data 9",
+        "Some data 10",
+        "Some data 11",
+        "Some data 12",
+        "Some data 13",
+        "Some data 14",
+        "Some data 15",
+        "Some data 16",
+        "Some data 17",
+        "Some data 18",
+        "Some data 19",
+        "Some data 20"
+    ]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myData.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("My Cell", forIndexPath: indexPath) as! MyTableViewCell
+        
+        let row = indexPath.row
+        
+        cell.myTextLabel.text = myData[row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, didHighlightRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.contentView.backgroundColor = UIColor.redColor()
+    }
+    
+    func tableView(tableView: UITableView, didUnhighlightRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.contentView.backgroundColor = UIColor.clearColor()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        cell?.contentView.backgroundColor = UIColor.grayColor()
+    }
+}
+

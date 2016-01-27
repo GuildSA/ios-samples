a9725939ab31276c6529c2e3b6297b47b554840f
Kevin Harris <kharris@guildsa.org>
2016-01-26 23:48:23 -0600

    Add new samples.

---
+++ PickerView/PickerView/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by Kevin Harris on 1/26/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    var pickerColors = ["White", "Red", "Green", "Blue", "Purple", "Orange", "Brown"];
+    
+    var pickerDates = [
+        ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        ["1", "2", "3", "4" , "5", "6" , "7", "8" , "9", "10" , "11", "12" , "13", "14" , "15",
+        "16" , "17", "18", "19", "20" , "21", "22" , "23", "24" , "25", "26" , "27", "28" , "29", "30" , "31"],
+        ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // From the UIPickerViewDataSource protocol.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        if(pickerView.tag == 1) {
+            return 1
+        } else if(pickerView.tag == 2) {
+            return 3
+        }
+        
+        return 1
+    }
+    
+    // From the UIPickerViewDataSource protocol.
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if(pickerView.tag == 1) {
+            return pickerColors.count
+        } else if(pickerView.tag == 2) {
+            return pickerDates[component].count
+        }
+        
+        return 0
+    }
+    
+    // From the UIPickerViewDataSource protocol.
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if(pickerView.tag == 1) {
+            return pickerColors[row]
+        } else if(pickerView.tag == 2) {
+            return pickerDates[component][row]
+        }
+        
+        return ""
+    }
+    
+    // From the UIPickerViewDelegate protocol.
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        if(pickerView.tag == 1) {
+
+            if(row == 0) {
+                self.view.backgroundColor = UIColor.whiteColor()
+            } else if(row == 1) {
+                self.view.backgroundColor = UIColor.redColor()
+            } else if(row == 2) {
+                self.view.backgroundColor = UIColor.greenColor()
+            } else if(row == 3) {
+                self.view.backgroundColor = UIColor.blueColor()
+            } else if(row == 4) {
+                self.view.backgroundColor = UIColor.purpleColor()
+            } else if(row == 5) {
+                self.view.backgroundColor = UIColor.orangeColor()
+            } else if(row == 6) {
+                self.view.backgroundColor = UIColor.brownColor()
+            } else {
+                self.view.backgroundColor = UIColor.lightGrayColor()
+            }
+            
+        } else if(pickerView.tag == 2) {
+            
+            print("\(pickerDates[component][row])")
+        }
+    }
+}
+
---
+++ UserDefaults/UserDefaults/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by Kevin Harris on 1/26/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func touchUpInsideRead(sender: UIButton) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        
+        let age = defaults.integerForKey("AGE")
+        print("AGE = \(age)")
+        
+        
+        let signedIntoFacebook = defaults.boolForKey("SIGNED_INTO_FACEBOOK")
+        print("SIGNED_INTO_FACEBOOK = \(signedIntoFacebook)")
+        
+        
+        if let homeTown = defaults.stringForKey("HOME_TOWN") {
+            print("HOME_TOWN = \(homeTown)")
+        } else {
+            print("HOME_TOWN has never been set!")
+        }
+        
+        
+        if let phoneNumber = defaults.stringForKey("PHONE_NUMBER") {
+            print("PHONE_NUMBER = \(phoneNumber)")
+        } else {
+            print("PHONE_NUMBER has never been set!")
+        }
+        
+        
+        let searchTermsData = defaults.objectForKey("SEARCH_TERMS") as? NSData
+        
+        if let searchTermsData = searchTermsData {
+            
+            let searchTermsArray = NSKeyedUnarchiver.unarchiveObjectWithData(searchTermsData) as? [String]
+            
+            if let searchTermsArray = searchTermsArray {
+                
+                for searchTerm in searchTermsArray {
+                    
+                    print(searchTerm)
+                }
+            }
+            
+        } else {
+            print("SEARCH_TERMS has never been set!")
+        }
+    }
+    
+    @IBAction func touchUpInsideWrite(sender: UIButton) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        defaults.setObject(24, forKey: "AGE")
+        
+        defaults.setObject(true, forKey: "SIGNED_INTO_FACEBOOK")
+        
+        defaults.setObject("Dallas", forKey: "HOME_TOWN")
+        
+        defaults.setObject("555-123-4567", forKey: "PHONE_NUMBER")
+
+        let searchTermsArray = ["Irish Pubs", "Concerts Frisco", "Coffee Shops"]
+        let searchTermsData = NSKeyedArchiver.archivedDataWithRootObject(searchTermsArray)
+        NSUserDefaults.standardUserDefaults().setObject(searchTermsData, forKey: "SEARCH_TERMS")
+        
+        defaults.synchronize()
+    }
+}
+

abf2080aaba9706558208de73edea1c519d7f83b
Kevin Harris <kharris@guildsa.org>
2016-08-10 11:36:22 -0500

    Add sample on creating custom controls.

---
+++ CustomControl/CustomControl/RatingControl.swift
@@ -0,0 +1,96 @@
+//
+//  RatingControl.swift
+//  CustomControl
+//
+//  Created by Kevin Harris on 8/10/16.
+//  Copyright © 2016 Kevin Harris. All rights reserved.
+//
+
+import UIKit
+
+// This custom control is based on a sub-section of an Apple tutorial
+// called "Start Developing iOS Apps (Swift)"
+// https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson5.html#//apple_ref/doc/uid/TP40015214-CH19-SW1
+
+class RatingControl: UIView {
+
+    // MARK: Properties
+    
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        
+        let emptyStarImage = UIImage(named: "emptyStar")
+        let filledStarImage = UIImage(named: "filledStar")
+        
+        for _ in 0..<5 {
+            let button = UIButton()
+            
+            button.setImage(emptyStarImage, forState: .Normal)
+            button.setImage(filledStarImage, forState: .Selected)
+            button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+            
+            button.adjustsImageWhenHighlighted = false
+            
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
+    }
+
+    override func layoutSubviews() {
+        
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus some spacing.
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    // To tell the stack view how to lay out our button, we also
+    // need to provide an intrinsic content size for it. To do this, 
+    // override the intrinsicContentSize method to match the size you 
+    // specified in Interface Builder like this:
+    override func intrinsicContentSize() -> CGSize {
+        
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    // MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        
+        rating = ratingButtons.indexOf(button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerate() {
+            // If the index of a button is less than the rating, that button shouldn't be selected.
+            button.selected = index < rating
+        }
+    }
+}
---
+++ CustomControl/CustomControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomControl
+//
+//  Created by Kevin Harris on 8/10/16.
+//  Copyright © 2016 Kevin Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+

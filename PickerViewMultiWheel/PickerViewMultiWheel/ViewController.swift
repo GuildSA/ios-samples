eb89323149fee5eb9fb5aeb59cd8cd8bde06344e
Kevin Harris <kharris@guildsa.org>
2016-09-08 23:26:47 -0500

    Split PickerView sample into two samples.

--- PickerViewMultiWheel/PickerViewMultiWheel/ViewController.swift
+++ PickerViewMultiWheel/PickerViewMultiWheel/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  PickerView
+//  PickerViewMultiWheel
 //
 //  Created by Kevin Harris on 1/26/16.
 //  Copyright © 2016 Guild/SA. All rights reserved.
@@ -9,8 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-
-    var pickerColors = ["White", "Red", "Green", "Blue", "Purple", "Orange", "Brown", "Yellow"];
+    
+    var pickerDates = [
+        
+        ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        
+        ["1", "2", "3", "4" , "5", "6" , "7", "8" , "9", "10" , "11", "12" , "13", "14",
+            "15", "16" , "17", "18", "19", "20" , "21", "22" , "23", "24" , "25", "26",
+            "27", "28" , "29", "30" , "31"],
+        
+        ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,35 +34,25 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     // From the UIPickerViewDataSource protocol.
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         
-        return 1
+        return 3
     }
     
     // From the UIPickerViewDataSource protocol.
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
-        return pickerColors.count
+        return pickerDates[component].count
     }
     
     // From the UIPickerViewDataSource protocol.
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        return pickerColors[row]
+        return pickerDates[component][row]
     }
     
     // From the UIPickerViewDelegate protocol.
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 
-        switch row {
-            case 0: self.view.backgroundColor = UIColor.whiteColor()
-            case 1: self.view.backgroundColor = UIColor.redColor()
-            case 2: self.view.backgroundColor = UIColor.greenColor()
-            case 3: self.view.backgroundColor = UIColor.blueColor()
-            case 4: self.view.backgroundColor = UIColor.purpleColor()
-            case 5: self.view.backgroundColor = UIColor.orangeColor()
-            case 6: self.view.backgroundColor = UIColor.brownColor()
-            case 7: self.view.backgroundColor = UIColor.yellowColor()
-            default: self.view.backgroundColor = UIColor.lightGrayColor()
-        }
+        print("\(pickerDates[component][row])")
     }
 }
 

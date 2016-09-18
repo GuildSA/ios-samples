3d0a814e85ffdc882bd43c46e2359d54c50bc343
Kevin Harris <kharris@guildsa.org>
2016-09-18 13:46:09 -0500

    Update to Swift 3.

--- GrandCentralDispatch/GrandCentralDispatch/ViewController.swift
+++ GrandCentralDispatch/GrandCentralDispatch/ViewController.swift
@@ -61,34 +61,30 @@ class ViewController: UIViewController {
     
     // Notes on calling dispatch_async and dispatch_get_global_queue:
     
-    // QOS_CLASS_USER_INTERACTIVE: The user interactive class represents tasks
+    // DispatchQoS.QoSClass.userInteractive: The user interactive class represents tasks
     // that need to be done immediately in order to provide a nice user experience.
     // Use it for UI updates, event handling and small workloads that require
     // low latency. The total amount of work done in this class during the
     // execution of your app should be small.
     
-    // QOS_CLASS_USER_INITIATED: The user initiated class represents tasks that
+    // DispatchQoS.QoSClass.userInitiated: The user initiated class represents tasks that
     // are initiated from the UI and can be performed asynchronously. It should
     // be used when the user is waiting for immediate results, and for tasks
     // required to continue user interaction.
     
-    // QOS_CLASS_UTILITY: The utility class represents long-running tasks, typically
+    // DispatchQoS.QoSClass.utility: The utility class represents long-running tasks, typically
     // with a user-visible progress indicator. Use it for computations, I/O,
     // networking, continuous data feeds and similar tasks. This class is designed
     // to be energy efficient.
     
-    // QOS_CLASS_BACKGROUND: The background class represents tasks that the user
+    // DispatchQoS.QoSClass.background: The background class represents tasks that the user
     // is not directly aware of. Use it for prefetching, maintenance, and other
     // tasks that don’t require user interaction and aren’t time-sensitive.
-    
-    // While the dispatch_get_global_queue() takes 2 parameters, the second one
-    // is always 0. Apple might make use of that second parameter in the future,
-    // but for now just put a 0 there. And the usage of [unowned self] in the
-    // closure is to avoid strong reference cycles.
+
     
     func dispatchAsync1() {
         
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { [unowned self] in
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async { [unowned self] in
             
             // Everything inside this closure will be executed on a new background thread.
             
@@ -101,7 +97,7 @@ class ViewController: UIViewController {
     
     func dispatchAsync2() {
         
-        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { [unowned self] in
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async { [unowned self] in
             
             // Everything inside this closure will be executed on a new background thread.
             

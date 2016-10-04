8d8fd7a4b9db5fb273d4b59160c731c3b617d4b2
Kevin Harris <kharris@guildsa.org>
2016-10-03 23:52:29 -0500

    Add sample for setting up Push Notifications under  Backendless.

---
+++ BackendlessPush/BackendlessPush/AppDelegate.swift
@@ -0,0 +1,95 @@
+//
+//  AppDelegate.swift
+//  BackendlessPush
+//
+//  Created by Kevin Harris on 10/3/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let VERSION_NUM = "v1"
+    
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Replace these with YOUR App's ID and Secret Key from YOUR Backendless Dashboard!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    let APP_ID = "761FB252-4CE5-99EF-FF77-C9E99ACAA400"
+    let SECRET_KEY = "926B9573-E688-8918-FF64-584F1E91EF00"
+    
+    var window: UIWindow?
+
+    let backendless = Backendless.sharedInstance()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        if APP_ID != "<replace-with-your-app-id>" && SECRET_KEY != "<replace-with-your-secret-key>" {
+            
+            backendless?.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
+            
+            let settings = UIUserNotificationSettings(types:[.alert, .sound, .badge], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            
+            backendless?.messaging.registerForRemoteNotifications()
+        }
+        
+        return true
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        backendless!.messaging.applicationWillTerminate()
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        backendless!.messaging.didRegisterForRemoteNotifications(withDeviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        
+        backendless!.messaging.didFailToRegisterForRemoteNotificationsWithError(error)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        
+        backendless!.messaging.didReceiveRemoteNotification(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        print("didReceiveRemoteNotification called!")
+        print("\(userInfo)")
+        
+        backendless!.messaging.didReceiveRemoteNotification(userInfo)
+        
+        completionHandler(.newData)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+}
+
---
+++ BackendlessPush/BackendlessPush/ViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  BackendlessPush
+//
+//  Created by Kevin Harris on 10/3/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, IBEPushReceiver {
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Don't forget to replace the App's ID and Secret Key in AppDelegate with YOUR own
+    // from YOUR Backendless Dashboard!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    let backendless = Backendless.sharedInstance()!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        backendless.messaging.pushReceiver = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func checkForBackendlessSetup() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        if appDelegate.APP_ID == "<replace-with-your-app-id>" || appDelegate.SECRET_KEY == "<replace-with-your-secret-key>" {
+            
+            let alertController = UIAlertController(title: "Backendless Error",
+                                                    message: "To use this sample you must register with Backendless, create an app, and replace the APP_ID and SECRET_KEY in the AppDelegate with the values from your app's settings.",
+                                                    preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    // IBEPushReceiver Methods
+
+    public func didReceiveRemoteNotification(_ notification: String!, headers: [AnyHashable : Any]!) {
+
+        print("Received Message: \(notification!)")
+        
+        let publisherName = headers["publisher_name"] ?? "nil"
+        let iosBadge = headers["ios-badge"] ?? "nil"
+        let iosSound = headers["ios-sound"] ?? "nil"
+        
+        print("publisher_name = \(publisherName)")
+        print("ios-badge = \(iosBadge)")
+        print("ios-sound = \(iosSound)")
+        //print("android-content-text = \(headers["android-content-text"]!)")
+        //print("android-content-title = \(headers["android-content-title"]!)")
+        //print("android-ticker-text = \(headers["android-ticker-text"]!)")
+        
+        let alertController = UIAlertController(title: "Push Notification",
+                                                message: "Our app received a Remote Push Notification! Here's our opportunity to do something special based on it.\n\nPush Data:\n\nnotification = \(notification!)\npublisher_name = \(publisherName)\nios-badge = \(iosBadge)\nios-sound = \(iosSound)",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(okAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    public func didRegisterForRemoteNotifications(withDeviceId deviceId: String!, fault: Fault!) {
+
+        if fault == nil {
+            print("didRegisterForRemoteNotificationsWithDeviceId: \(deviceId!)")
+        } else {
+            print("didRegisterForRemoteNotificationsWithDeviceId: \(fault)")
+        }
+    }
+
+    public func didFailToRegisterForRemoteNotificationsWithError(_ err: Error!) {
+
+        print("didFailToRegisterForRemoteNotificationsWithError: \(err)")
+    }
+    
+    @IBAction func triggerPushFromAppBtn(_ sender: UIButton) {
+        
+        checkForBackendlessSetup()
+        
+        let publishHeaders = [
+            "publisher_name": "App",
+            "ios-badge": "1",
+            "ios-sound": "default"
+            //"android-content-text": "Content text",
+            //"android-content-title": "Title",
+            //"android-ticker-text": "Ticker text"
+        ]
+        
+        let publishOptions = PublishOptions()
+        publishOptions.assignHeaders(publishHeaders)
+        
+        let deliveryOptions = DeliveryOptions()
+        deliveryOptions.pushPolicy(PUSH_ONLY)
+        deliveryOptions.pushBroadcast(FOR_ANDROID.rawValue|FOR_IOS.rawValue)
+        
+        // If you know the Device ID, we could send the push to just that one device.
+        //deliveryOptions.addSinglecast("A44ECEC0-B48F-4B5C-81C8-CF97ED5B59B4")
+        
+        backendless.messaging.publish(
+            "default", // Which channel to target!
+            message: "Push triggered by app!",
+            publishOptions: publishOptions,
+            deliveryOptions: deliveryOptions,
+            response: {(status: MessageStatus?) -> () in
+                print("Message has been sent: \(status!)")
+            },
+            error: { (fault : Fault?) -> () in
+                print("Server reported an error: \(fault)")
+        })
+    }
+}
+

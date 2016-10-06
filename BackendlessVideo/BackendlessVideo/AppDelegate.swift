cdbd1373d6194b1b41ebd0a96b42c813eabfc97a
Kevin Harris <kharris@guildsa.org>
2016-10-06 11:45:31 -0500

    Add new sample to demo video recording, playback and live streaming under Backendless.

---
+++ BackendlessVideo/BackendlessVideo/AppDelegate.swift
@@ -0,0 +1,110 @@
+//
+//  AppDelegate.swift
+//  BackendlessVideo
+//
+//  Created by Kevin Harris on 10/06/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Replace these with YOUR App's ID and Secret Key from YOUR Backendless Dashboard!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    let APP_ID = "<replace-with-your-app-id>"
+    let SECRET_KEY = "<replace-with-your-secret-key>"
+
+    let VERSION_NUM = "v1"
+    
+    let EMAIL = "test@gmail.com" // Doubles as User Name
+    let PASSWORD = "password"
+    
+    let backendless = Backendless.sharedInstance()
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        // Override point for customization after application launch.
+        if APP_ID != "<replace-with-your-app-id>" && SECRET_KEY != "<replace-with-your-secret-key>" {
+            
+            backendless?.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
+            backendless?.userService.setStayLoggedIn(true)
+            backendless?.setThrowException(false)
+            
+            backendless?.hostURL = "http://api.backendless.com"
+            
+            backendless?.mediaService = MediaService()
+            
+            // Next, check if the user is already logged in. If they are - do nothing else!
+            let isValidUser = backendless?.userService.isValidUserToken()
+            
+            if isValidUser != nil && isValidUser != 0 {
+                
+                // The user has a valid user token so we know for sure the user is already logged!
+                print("User is already logged: \(isValidUser?.boolValue)");
+                
+            } else {
+                
+                // If the user is not logged in, register the test user,
+                // and if that suceeds, go ahead and log them in.
+                
+                let user: BackendlessUser = BackendlessUser()
+                user.email = EMAIL as NSString!
+                user.password = PASSWORD as NSString!
+                
+                backendless?.userService.registering( user,
+                                                     
+                response: { (user: BackendlessUser?) -> Void in
+                    
+                        print("User was registered: \(user?.objectId)")
+                    
+                        self.backendless?.userService.login( self.EMAIL, password: self.PASSWORD,
+        
+                            response: { (user: BackendlessUser?) -> Void in
+                                print("User logged in: \(user!.objectId)")
+                            },
+            
+                           error: { (fault: Fault?) -> Void in
+                                print("User failed to login: \(fault)")
+                            }
+                        )
+                    },
+                                                         
+                    error: { (fault: Fault?) -> Void in
+                        print("User failed to register: \(fault)")
+                    }
+                )
+            }
+        }
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ BackendlessVideo/BackendlessVideo/ViewController.swift
@@ -0,0 +1,381 @@
+//
+//  ViewController.swift
+//  BackendlessVideo
+//
+//  Created by Kevin Harris on 10/06/16.
+//  Copyright © 2016 Guild/SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate, IMediaStreamerDelegate {
+    
+    @IBOutlet var publishBtn: UIButton!
+    @IBOutlet var playbackBtn: UIButton!
+    @IBOutlet var stopMediaBtn: UIButton!
+    @IBOutlet var swapCameraBtn: UIButton!
+    @IBOutlet var preView: UIView!
+    @IBOutlet var playbackView: UIImageView!
+    @IBOutlet var streamNameTextField: UITextField!
+    @IBOutlet var netActivity: UIActivityIndicatorView!
+    @IBOutlet weak var videoModeSegment: UISegmentedControl!
+    @IBOutlet weak var publishOptionsSegment: UISegmentedControl!
+    
+    var isLive = false
+    
+    var backendless = Backendless.sharedInstance()
+    var publisher: MediaPublisher?
+    var player: MediaPlayer?
+    let VIDEO_TUBE = "Default"
+    
+    enum VideoMode {
+        
+        case recordAndPlayback
+        case liveStream
+        case viewStream
+    }
+    
+    var currentVideoMode: VideoMode = .recordAndPlayback
+    
+    enum PublishOptions {
+        
+        case videoPlusAudio
+        case videoOnly
+        case audioOnly
+    }
+    
+    var currentPublishOptions: PublishOptions = .videoPlusAudio
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        streamNameTextField.addTarget(self, action: #selector(textFieldChanged(textField:)), for: UIControlEvents.editingChanged)
+        
+        self.publishBtn.isEnabled = false
+        self.playbackBtn.isEnabled = false
+        self.swapCameraBtn.isEnabled = false
+        self.stopMediaBtn.isEnabled = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldChanged(textField: UITextField) {
+        
+        if streamNameTextField.text == "" {
+            
+            playbackBtn.isEnabled = false
+            stopMediaBtn.isEnabled = false
+            publishBtn.isEnabled = false
+            swapCameraBtn.isEnabled = false
+
+        } else {
+            
+            switch currentVideoMode {
+                
+                case .recordAndPlayback:
+                    
+                    playbackBtn.isEnabled = true
+                    publishBtn.isEnabled = true
+                    
+                case .liveStream:
+
+                    playbackBtn.isEnabled = false
+                    publishBtn.isEnabled = true
+                    
+                case .viewStream:
+                    
+                    playbackBtn.isEnabled = true
+                    publishBtn.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func onSwitchCamerasBtn(_ sender: UIButton) {
+        
+        publisher?.switchCameras()
+    }
+    
+    @IBAction func videoModeChanged(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            currentVideoMode = .recordAndPlayback
+        } else if sender.selectedSegmentIndex == 1 {
+            currentVideoMode = .liveStream
+        } else if sender.selectedSegmentIndex == 2 {
+            currentVideoMode = .viewStream
+        }
+        
+        refreshUI()
+    }
+    
+    @IBAction func publishOptionsChanged(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            currentPublishOptions = .videoPlusAudio
+        } else if sender.selectedSegmentIndex == 1 {
+            currentPublishOptions = .videoOnly
+        } else if sender.selectedSegmentIndex == 2 {
+            currentPublishOptions = .audioOnly
+        }
+    }
+    
+    func refreshUI() {
+        
+        streamNameTextField.isEnabled = true
+        videoModeSegment.isEnabled = true
+        publishOptionsSegment.isEnabled = true
+        
+        switch currentVideoMode {
+            
+            case .recordAndPlayback:
+                
+                isLive = false
+                
+                if streamNameTextField.text == "" {
+                    publishBtn.isEnabled = false
+                    playbackBtn.isEnabled = false
+                } else {
+                    publishBtn.isEnabled = true
+                    playbackBtn.isEnabled = true
+                }
+                
+                swapCameraBtn.isEnabled = false
+                stopMediaBtn.isEnabled = false
+                
+                preView.isHidden = false
+                playbackView.isHidden = true
+            
+                publishBtn.setTitle("Record", for: .disabled)
+                publishBtn.setTitle("Record", for: .normal)
+                
+            case .liveStream:
+                
+                isLive = true
+                
+                if streamNameTextField.text == "" {
+                    publishBtn.isEnabled = false
+                } else {
+                    publishBtn.isEnabled = true
+                }
+                
+                playbackBtn.isEnabled = false
+                swapCameraBtn.isEnabled = false
+                stopMediaBtn.isEnabled = false
+                
+                preView.isHidden = false
+                playbackView.isHidden = true
+            
+                publishBtn.setTitle("Stream", for: .disabled)
+                publishBtn.setTitle("Stream", for: .normal)
+                
+            case .viewStream:
+                
+                isLive = true
+                
+                publishBtn.isEnabled = false
+                
+                if streamNameTextField.text == "" {
+                    playbackBtn.isEnabled = false
+                } else {
+                    playbackBtn.isEnabled = true
+                }
+                
+                swapCameraBtn.isEnabled = false
+                stopMediaBtn.isEnabled = false
+                
+                preView.isHidden = true
+                playbackView.isHidden = false
+            
+                publishBtn.setTitle("Stream", for: .disabled)
+                publishBtn.setTitle("Stream", for: .normal)
+        }
+    }
+    
+    @IBAction func onStopBtn(_ sender: UIButton) {
+        
+        stopMedia()
+    }
+    
+    func stopMedia() {
+        
+        if publisher != nil {
+            
+            publisher?.disconnect()
+            publisher = nil;
+        }
+        
+        if player != nil {
+            
+            player?.disconnect()
+            player = nil;
+        }
+        
+        refreshUI()
+        
+        netActivity.stopAnimating()
+    }
+    
+    @IBAction func onPlayBtn(_ sender: UIButton) {
+        
+        var options: MediaPlaybackOptions
+        
+        if isLive {
+            options = MediaPlaybackOptions.liveStream(self.playbackView) as! MediaPlaybackOptions
+        } else {
+            options = MediaPlaybackOptions.recordStream(self.playbackView) as! MediaPlaybackOptions
+        }
+        
+        options.orientation = .up
+        options.isRealTime = isLive
+        
+        player = backendless?.mediaService.playbackStream(streamNameTextField.text, tube: VIDEO_TUBE, options: options, responder: self)
+        
+        publishBtn.isEnabled = false
+        playbackBtn.isEnabled = false
+        streamNameTextField.isEnabled = false
+        videoModeSegment.isEnabled = false
+        publishOptionsSegment.isEnabled = false
+
+        netActivity.startAnimating()
+    }
+    
+    @IBAction func onRecordBtn(_ sender: UIButton) {
+        
+        var options: MediaPublishOptions
+        
+        if isLive {
+            options = MediaPublishOptions.liveStream(self.preView) as! MediaPublishOptions
+        } else {
+            options = MediaPublishOptions.recordStream(self.preView) as! MediaPublishOptions
+        }
+        
+        switch currentPublishOptions {
+                
+            case .videoPlusAudio:
+                
+                options.orientation = .portrait
+                
+                //options.resolution = RESOLUTION_LOW         // 144x192px (landscape) & 192x144px (portrait)
+                options.resolution = RESOLUTION_CIF         // 288x352px (landscape) & 352x288px (portrait)
+                //options.resolution = RESOLUTION_MEDIUM      // 360x480px (landscape) & 480x368px (portrait)
+                //options.resolution = RESOLUTION_VGA         // 480x640px (landscape) & 640x480px (portrait)
+                //options.resolution = RESOLUTION_HIGH        // 720x1280px (landscape) & 1280x720px (portrait)
+                
+                options.content = AUDIO_AND_VIDEO
+                
+            case .videoOnly:
+
+                options.orientation = .portrait
+                options.resolution = RESOLUTION_CIF
+                options.content = ONLY_VIDEO
+        
+            case .audioOnly:
+                
+                options.content = ONLY_AUDIO
+        }
+        
+        publisher = backendless?.mediaService.publishStream(streamNameTextField.text, tube: VIDEO_TUBE, options: options, responder: self)
+
+        publishBtn.isEnabled = false
+        playbackBtn.isEnabled = false
+        streamNameTextField.isEnabled = false
+        videoModeSegment.isEnabled = false
+        publishOptionsSegment.isEnabled = false
+        
+        netActivity.startAnimating()
+    }
+    
+    // MARK: UITextFieldDelegate protocol methods
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: IMediaStreamerDelegate protocol methods to handle stream state changes and errors
+    
+    func streamStateChanged(_ sender: Any!, state: Int32, description: String!) -> Void {
+        
+        switch state {
+            
+            case 0: //CONN_DISCONNECTED
+                
+                DispatchQueue.main.async {
+                    self.stopMedia()
+                }
+            
+            case 1: break //CONN_CONNECTED
+            
+            case 2: //CONN_CREATED
+                
+                DispatchQueue.main.async {
+                    self.stopMediaBtn.isEnabled = true
+                }
+                
+            case 3: //STREAM_PLAYING
+                
+                DispatchQueue.main.async {
+                    
+                    if self.publisher != nil {
+                        
+                        if description != "NetStream.Publish.Start" {
+                            self.stopMedia()
+                            return
+                        }
+                        
+                        self.swapCameraBtn.isEnabled = true
+                        self.netActivity.stopAnimating()
+                    }
+                    
+                    if self.player != nil {
+                        
+                        if description == "NetStream.Play.StreamNotFound" {
+                            self.stopMedia()
+                            return
+                        }
+                        
+                        if description != "NetStream.Play.Start" {
+                            return
+                        }
+                        
+                        MPMediaData.routeAudioToSpeaker()
+                        
+                        self.preView.isHidden = true
+                        self.playbackView.isHidden = false
+                        
+                        self.netActivity.stopAnimating()
+                    }
+                }
+                
+                return
+                
+            case 4: //STREAM_PAUSED
+                
+                DispatchQueue.main.async {
+                    
+                    //if description == "NetStream.Play.StreamNotFound" {
+                    //}
+                    
+                    self.stopMedia()
+                }
+            
+            default:
+                print("streamStateChanged unhandled state: \(state)");
+                return
+        }
+    }
+    
+    func streamConnectFailed(_ sender: Any!, code: Int32, description: String!) -> () {
+        
+        print("<IMediaStreamerDelegate> streamConnectFailed: \(code) = \(description)");
+        
+        stopMedia()
+    }
+}
+

e33a6825fd913d98ee1c7749ac32701197d16e12
Harris, Kevin [R3PI] <kevin.harris@r3pi.io>
2019-11-18 16:13:06 -0600

    Update.

--- JSQMessagesDemo/JSQMessagesDemo/ViewController.swift
+++ JSQMessagesDemo/JSQMessagesDemo/ViewController.swift
@@ -264,9 +264,12 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         })
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
         
-        guard let image = info[UIImagePickerControllerOriginalImage] as! UIImage? else { return }
+        guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as! UIImage? else { return }
         
         addImage(senderId: self.senderId, displayName: self.senderDisplayName, image: image)
         
@@ -276,4 +279,14 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             
         })
     }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
 }
